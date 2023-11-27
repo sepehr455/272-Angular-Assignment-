44bce733a1833e07bdf153c9df1e3f1a90c9fa3e
sepehr455 <84247783+sepehr455@users.noreply.github.com>
2023-11-26 19:35:25 -0800

    added the map component

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'leaflet-map',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+export class MapComponent implements OnInit {
+
+  private map!: L.Map;
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([49.2827, -123.1207], 13); // Coordinates for Vancouver
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+  }
+}
