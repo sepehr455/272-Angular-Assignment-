cbcebb8f90e68dec432c1e2a6937d63a306f80c7
sepehr455 <84247783+sepehr455@users.noreply.github.com>
2023-11-26 19:22:03 -0800

    initial project push

--- src/app/main/app.component.ts
+++ src/app/main/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MapComponent} from "../map/map.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MapComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'AngularAssignment';
