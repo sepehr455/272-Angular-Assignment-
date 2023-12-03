d93257d5162eafa8db2ec58131b9c47f7766fe1f
sepehr455 <84247783+sepehr455@users.noreply.github.com>
2023-12-03 12:19:40 -0800

    now using uuid instead of date

--- src/app/Report.ts
+++ src/app/Report.ts
@@ -1,7 +1,10 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export type Location = {
   name: string;
   xCoord: number;
   yCoord: number;
+  numberOfReports: number;
 }
 
 export class Report {
@@ -17,8 +20,7 @@ export class Report {
   image: string;
 
   constructor(baddieName: string, location: Location, reporterName: string, reporterPhone: string, reportDate: string, reportTime: string, status: "Open" | "Resolved", extraInfo: string, image: string) {
-    this.id = `id_${new Date().toISOString().toLowerCase().replace(/[\W_]+/g, '-')}`;
-    this.baddieName = baddieName;
+    this.id = uuidv4();    this.baddieName = baddieName;
     this.location = location;
     this.reporterName = reporterName;
     this.reporterPhone = reporterPhone;
